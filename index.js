4a30a882c4070a94c13d3a148751288a3a2d2a6a
Joy <joyumoekpo1@gmail.com>
2021-03-08 17:18:38 -0600

    Added mysql connection

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "GoodizLife85^",
+    database: "managementDB"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    firstPrompt();
+  });
