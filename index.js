2c3c176afa7a8efaefd45bb350dfe1805cc5db4f
Joy <joyumoekpo1@gmail.com>
2021-03-08 17:57:21 -0600

    Added switch case

--- index.js
+++ index.js
@@ -27,8 +27,33 @@ function start() {
             "Remove Employees",
             "Update Employee Role",
             "Add Role",
-            "End"]
-})
+            "Exit"]
+}).then(function ({task}) {
+    switch (task) {
+        case "View Employees":
+            employee();
+            break;
+        case "View Employees by Department":
+            employeeDepartment();
+            break;
+        case "Add Employee":
+            employeeAdd();
+            break;
+        case "Remove Employees":
+            employeesRemove();
+            break;
+        case "Update Employee Role":
+            employeeUpdate();
+            break;
+        case "Add Role":
+            roleAdd();
+            break;
+        case "Exit":
+            connection.end();
+            break;
+    }
+});
 };
+
 
 start();
